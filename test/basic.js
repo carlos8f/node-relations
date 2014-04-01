ff3904cf49af69fbf8c0c72c3ade44e0e3c052c2
Jan Zaloudek <honzicekz@gmail.com>
2014-04-01 11:30:31 +0200

    removed skip from tests

--- test/basic.js
+++ test/basic.js
@@ -1,13 +1,13 @@
-describe.skip('memory store', function () {
+describe('memory store', function () {
   doBasicTest();
 });
 
-describe.skip('mysql store', function () {
+describe('mysql store', function () {
   var mysql = require('mysql');
   doBasicTest('mysql', {client: mysql.createConnection({user: 'root', database: 'test'})});
 });
 
-describe.skip('redis store', function () {
+describe('redis store', function () {
   var redis = require('redis');
   doBasicTest('redis', {client: redis.createClient(6379, '95.168.218.4'), prefix: 'relations-test:test-prefix'});
 });
